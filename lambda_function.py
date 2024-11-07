7f8c5cf375bf08b6749e31aee96797278006e559
Han-16 <haanbk16@gmail.com>
2024-11-08 03:50:23 +0900

    feat: first commit

---
+++ lambda_function.py
@@ -0,0 +1,126 @@
+import os
+import json
+import boto3
+from STT.ClovaText import make_stt_txt
+from Chunking.EmbeddingChunking import make_chunk
+from Keywords.BllossomKeyword_to_md import generate_summary_jsons, generate_report_from_json
+from Diagrams.DiagramGeneration import diagram_gen
+
+# S3 클라이언트 초기화
+ACCESS_KEY = os.environ.get("ACCESS_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+bucket_name = "clerker-ai.bucket"
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY
+    )
+
+def download_from_s3(s3_path, local_path):
+    s3.download_file(bucket_name, s3_path, local_path)
+
+def upload_to_s3(local_path, s3_path):
+    s3.upload_file(local_path, bucket_name, s3_path)
+
+def lambda_handler(event, context):
+    # Lambda 핸들러 함수
+
+    # 입력 이벤트에서 도메인과 mp3 파일 URL을 가져옵니다.
+    input_domain = event.get('domain', 'IT')
+    mp3_file_url = event.get('mp3FileUrl')
+
+    # 파일 경로 설정
+    input_audio_file = '/tmp/STT/stt_audio/input_audio.mp3'
+    output_txt_file = '/tmp/STT/stt_text/stt_text.txt'
+    output_chunk_dict = '/tmp/Chunking/chunking_text.json'
+    output_summary_json = '/tmp/Keywords/summary.json'
+    diagram_summary_json = '/tmp/Diagrams/diagram_summary.json'
+    output_report_md = '/tmp/Keywords/report.md'
+    s3_font_path = 'Keywords/NanumFontSetup_TTF_SQUARE_ROUND/NanumSquareRoundB.ttf'
+    font_path = f'/tmp/{s3_font_path}'
+
+    # 1. S3에서 mp3 파일 다운로드
+    # mp3_file_url은 S3의 키로 간주합니다.
+    download_from_s3(mp3_file_url, input_audio_file)
+
+    # 2. Keyword Boosting JSON 파일 다운로드
+    keyword_boosting_domain = f'STT/stt_text/KeywordBoosting/{input_domain}_KeywordBoosting.json'
+    keyword_boosting_agenda = 'STT/stt_text/KeywordBoosting/Agenda_middle.json'
+    local_keyword_boosting_domain = f'/tmp/STT/stt_text/KeywordBoosting/{input_domain}_KeywordBoosting.json'
+    local_keyword_boosting_agenda = '/tmp/STT/stt_text/KeywordBoosting/Agenda_middle.json'
+
+    download_from_s3(keyword_boosting_domain, local_keyword_boosting_domain)
+    download_from_s3(keyword_boosting_agenda, local_keyword_boosting_agenda)
+
+    # 3. 모델 파일 다운로드
+    local_model_dir = '/tmp/models'
+    os.makedirs(local_model_dir, exist_ok=True)
+    # 모델 파일 목록을 정의합니다.
+    model_files = [
+        'models--jhgan--ko-sroberta-sts/snapshots/3efa8e54a06798b00bd1abb9c22b2dd530e22b24',
+        'models--MLP-KTLim--llama-3-Korean-Bllossom-8B-gguf-Q4_K_M/snapshots/4e602ad115392e7298674e092d6f8b45138f1db7',
+        'models--MLP-KTLim--llama-3-Korean-Bllossom-8B-gguf-Q4_K_M/llama-3-Korean-Bllossom-8B-Q4_K_M.gguf',
+    ]
+    for model_file in model_files:
+        s3_model_path = f'models/{model_file}'
+        local_model_path = os.path.join(local_model_dir, os.path.basename(model_file))
+        download_from_s3(s3_model_path, local_model_path)
+    
+    download_from_s3(s3_font_path, font_path)
+    # 4. STT 파일 생성
+    make_stt_txt(
+        input_domain,
+        input_audio_file,
+        output_txt_file,
+        local_keyword_boosting_domain,
+        local_keyword_boosting_agenda
+    )
+    print(f"STT 파일 생성 완료 : {output_txt_file}")
+
+    # 5. Chunk 생성
+    make_chunk(output_txt_file, output_chunk_dict)
+    print(f"Chunk Dict 파일 생성 완료 : {output_chunk_dict}")
+
+    # 6. 요약 및 다이어그램용 JSON 파일 생성
+    generate_summary_jsons(
+        output_chunk_dict,
+        diagram_summary_json,
+        output_summary_json,
+        model_id=os.path.join(local_model_dir, 'models--MLP-KTLim--llama-3-Korean-Bllossom-8B-gguf-Q4_K_M'),
+        model_path=os.path.join(local_model_dir, 'models--MLP-KTLim--llama-3-Korean-Bllossom-8B-gguf-Q4_K_M/llama-3-Korean-Bllossom-8B-Q4_K_M.gguf')
+    )
+    print(f"다이어그램 및 보고서용 JSON 파일 생성 완료 : {output_summary_json}")
+
+    # 7. 다이어그램 생성
+    diagram_gen(diagram_summary_json)
+    print("다이어그램 생성 완료")
+
+    # 8. 보고서 생성
+    generate_report_from_json(output_summary_json, output_report_md)
+    print(f"Report 파일 생성 완료 : {output_report_md}")
+
+    # 9. 결과 파일을 S3에 업로드
+    upload_to_s3(output_txt_file, 'STT/stt_text/stt_text.txt')
+    upload_to_s3(output_chunk_dict, 'Chunking/chunking_text.json')
+    upload_to_s3(output_summary_json, 'Keywords/summary.json')
+    upload_to_s3(output_report_md, 'Keywords/report.md')
+    # 다이어그램 이미지들이 저장된 경로를 지정하여 업로드
+    diagram_images_dir = '/tmp/Diagrams/mermaid'
+    for root, dirs, files in os.walk(diagram_images_dir):
+        for file in files:
+            local_file_path = os.path.join(root, file)
+            s3_file_path = f'Diagrams/mermaid/{file}'
+            upload_to_s3(local_file_path, s3_file_path)
+
+    # 10. 응답 생성
+    response = {
+        "report": f"https://{bucket_name}.s3.amazonaws.com/Keywords/report.md",
+        "stt": f"https://{bucket_name}.s3.amazonaws.com/STT/stt_text/stt_text.txt",
+        "diagram_image": f"https://{bucket_name}.s3.amazonaws.com/Diagrams/mermaid.zip"
+    }
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(response)
+    }
